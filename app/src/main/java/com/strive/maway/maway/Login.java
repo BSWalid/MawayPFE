d497ca192cc8d52bad05ae55c56ca7dfec35e748
Walid <bensidiaissa.walid@gmail.com>
2018-03-28 18:54:18 +0200

    Signup is working like charm

--- app/src/main/java/com/strive/maway/maway/Login.java
+++ app/src/main/java/com/strive/maway/maway/Login.java
@@ -7,14 +7,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
     Button loginbtn;
     TextView sigupbtn;
+
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
 
         loginbtn = (Button) findViewById(R.id.login);
         sigupbtn = (TextView) findViewById(R.id.sigupbtn);
