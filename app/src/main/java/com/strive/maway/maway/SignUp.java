304e6a91d2ba203e4f05050e28c9beda56ea1aa5
Hildweig <Hildweig@gmail.com>
2018-03-29 12:14:39 +0200

    i changed a bit sign up functionality, it's not registred yet in our database yet since i did just the first part of that person's video, also when cloning you will have bugs because u will have to reconnect your project to firebase i think, cause i had to did the same

--- app/src/main/java/com/strive/maway/maway/SignUp.java
+++ app/src/main/java/com/strive/maway/maway/SignUp.java
@@ -2,11 +2,13 @@ package com.strive.maway.maway;
 
 import android.content.Intent;
 import android.nfc.Tag;
+import android.os.PatternMatcher;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,27 +23,33 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class SignUp extends AppCompatActivity {
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
     Button btnsignup ;
     EditText username;
     EditText password;
     EditText email;
+    private FirebaseAuth mAuth;
     private Firebase mRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
         Firebase.setAndroidContext(this);
         mRef = new Firebase("https://maway-1520842395181.firebaseio.com/Users");
 
-        btnsignup =(Button) findViewById(R.id.btnsignup);
+        //btnsignup =(Button) findViewById(R.id.btnsignup);
         username =(EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         email = (EditText) findViewById(R.id.email);
 
+        //instantiate auth
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.btnsignup).setOnClickListener(this);
 
-        btnsignup.setOnClickListener(new View.OnClickListener() {
+
+       /* btnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String emailText = email.getText().toString();
@@ -54,7 +62,7 @@ public class SignUp extends AppCompatActivity {
                  key.child("username").setValue(usernameText);
 
             }
-        });
+        }); */
 
 
 
@@ -63,11 +71,66 @@ public class SignUp extends AppCompatActivity {
 
 
     }
+ private void registerUser(){
 
+        String usernameText = username.getText().toString().trim();
+        String emailText = email.getText().toString().trim();
+        String passwordText = password.getText().toString().trim();
 
+        //check if email is not empty
 
+        if (emailText.isEmpty()){
+           email.setError("Email is required");
+           email.requestFocus(); //request the focus to the email
+           return; //it stops the execution
+        }
 
+     //check if email is valid
 
+     if(!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
+       email.setError("Please enter a valid email");
+       email.requestFocus(); //request the focus to the email
+       return; //it stops the execution
+     }
+     //check if password is empty
 
+     if(passwordText.isEmpty()){
+           password.setError("Password is required");
+           password.requestFocus(); //request the focus to the password
+           return; //it stops the execution
+     }
+
+     //check if password has a valid length
+
+     if(passwordText.length()<6) {
+         password.setError("Minimum length of password should be six");
+         password.requestFocus(); //request the focus to the password
+         return; //it stops the execution
+     }
+
+     //the complete Listener detects the completion, it will take a complete listener interface object
+     mAuth.createUserWithEmailAndPassword(emailText,passwordText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+         @Override
+         //when the registration is completed the method onComplete() is called
+         //here we can check if it was successful or not, using the task object
+         public void onComplete(@NonNull Task<AuthResult> task) {
+
+             if(task.isSuccessful()){
+                 Toast.makeText(getApplicationContext(),"You have been successfully registered",Toast.LENGTH_SHORT).show();
+             }
+         }
+     });
+
+ }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case (R.id.btnsignup):
+                  registerUser();
+                break;
+        }
+
+    }
 
 }
